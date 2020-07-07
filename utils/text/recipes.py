0089047556c0af973560f3cf0b6eb31043de9cfc
Avashna Govender <avashnagovender@Aria.turbospeed.home>
2020-07-07 10:49:04 +0200

    Creating copy of WaveRNN codebase

---
+++ utils/text/recipes.py
@@ -0,0 +1,20 @@
+from utils.files import get_files
+from pathlib import Path
+from typing import Union
+
+
+def ljspeech(path: Union[str, Path]):
+    #csv_file = get_files(path, extension='.csv')
+    filepath = os.path.join(path,"train.csv")
+    #assert len(csv_file) == 1
+
+    text_dict = {}
+
+    with open(filepath, encoding='utf-8') as f  :
+        content =  f.readlines()
+
+    for line in content :
+        split = line.split('|')
+        text_dict[split[0]] = split[-1]
+
+    return text_dict

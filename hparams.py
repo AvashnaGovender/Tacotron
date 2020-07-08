ba07b0dbf9226c9e31b727d93672b59b1be998fa
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-07-08 19:07:12 +0200

    updated wav path

--- hparams.py
+++ hparams.py
@@ -2,7 +2,7 @@
 # CONFIG -----------------------------------------------------------------------------------------------------------#
 ROOT = '/exports/csce/eddie/inf/groups/eddie_inf_hcrc_cstr_general/avashna/Samsung/'
 # Here are the input and output data paths (Note: you can override wav_path in preprocess.py)
-wav_path = ROOT+'data/wavs_train/CB_JE'
+wav_path = ROOT+'data/wavs_train/CB-JE'
 data_path = ROOT+'data/'
 
 # model ids are separate - that way you can use a new tts with an old wavernn and vice versa

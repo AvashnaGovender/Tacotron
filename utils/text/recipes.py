426321b8b2522cef97495c5ac37536611f4b8e1c
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-07-10 15:23:02 +0200

    updated read files

--- utils/text/recipes.py
+++ utils/text/recipes.py
@@ -2,13 +2,12 @@ from utils.files import get_files
 from pathlib import Path
 from typing import Union
 
-def blizzard(path: Union[str, Path]):
-    csv_file = get_files(path, extension='.csv')
-    assert len(csv_file) == 1
+def blizzard(path: Union[str, Path], books):
+    filepath = os.path.join(path,"train.csv")
 
     text_dict = {}
 
-    with open(csv_file[0], encoding='utf-8') as f  :
+    with open(filepath, encoding='utf-8') as f  :
         content =  f.readlines()
 
     for line in content :

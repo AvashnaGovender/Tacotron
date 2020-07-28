9ef7cf41917dd442afe500a0e3f238d998416185
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-07-28 16:30:12 +0200

    Update test_taco_model.py

--- test_taco_model.py
+++ test_taco_model.py
@@ -77,7 +77,7 @@ if __name__ == "__main__":
                          stop_threshold=hp.tts_stop_threshold).to(device)
 
 
-    tts_model.load('checkpoints/blizzard_baseline_JE.tacotron/taco_step228K_weights.pyt')
+    tts_model.load('checkpoints/blizzard_baseline_JE.tacotron/taco_step300K_weights.pyt')
 
     if input_text:
         inputs = [text_to_sequence(input_text.strip(), hp.tts_cleaner_names)]

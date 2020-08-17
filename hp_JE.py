0174105e5f0692be4f1d7d276bd72845f830d0eb
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-08-17 11:21:12 +0200

    updated training schedule

--- hp_JE.py
+++ hp_JE.py
@@ -82,8 +82,8 @@ tts_stop_threshold = -3.4           # Value below which audio generation ends.
 # Training
 
 tts_schedule = [(7,  1e-3,  10_000,  32),   # progressive training schedule
-                (5,  1e-4, 100_000,  32),   # (r, lr, step, batch_size)
-                (2,  1e-4, 180_000,  16),
+                (5,  1e-4, 122_038,  32),   # (r, lr, step, batch_size)
+                (2,  1e-4, 232_706,  16),
                 (2,  1e-4, 350_000,  8)]
 
 tts_max_mel_len = 1250              # if you have a couple of extremely long spectrograms you might want to use this

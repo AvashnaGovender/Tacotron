33727dedfabdc1d467e6861f4ce14361fd106ee6
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-09-01 13:23:21 +0200

    Update files.py

--- utils/files.py
+++ utils/files.py
@@ -9,12 +9,21 @@ def get_files(path, books, extension='.csv'):
 
     content  = [x.strip() for x in content ]
 
-    for x in content:
-        name  = x.split("|")[0]
-        book = name.split("-")
-        bookname = book[0]+"-"+book[1]
+    if len(books) != 0:
+        for x in content:
+            name  = x.split("|")[0]
+            book = name.split("-")
+            bookname = book[0]+"-"+book[1]
 
-        if bookname in books:
-            name = os.path.join(path, bookname, name)
-        filenames += [name]
+            if bookname in books:
+                name = os.path.join(path, bookname, name)
+            filenames += [name]
+    else:
+        for x in content:
+            name  = x.split("|")[0]
+            name = os.path.join(path,name)
+            filenames += [name]
+
+
+
     return filenames

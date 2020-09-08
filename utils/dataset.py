faf3f80ede5fd8a7504726f8ec9251722088d0f8
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-09-08 12:59:12 +0200

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -115,7 +115,7 @@ def get_tts_datasets(path: Path, batch_size, r):
         if len <= hp.tts_max_mel_len:
             print(item_id)
             print(file_ids[0])
-
+            exit()
             if item_id in file_ids:
                 dataset_ids += [item_id]
                 mel_lengths += [len]

c67053c25cdd8c41f53c26beaf804d87ae8116fd
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-09-13 12:57:24 +0200

    taco_generation

---
+++ auto_generate.sh
@@ -0,0 +1,3 @@
+python test_taco_model_auto.py --checkpoint prev/blizzard_baseline_JE.tacotron/final_taco_300K.pyt --model_name JE_fixed
+python test_taco_model_auto.py --checkpoint blizzard_baseline_speaking_rate.tacotron/taco_step300K_weights.pyt --model_name SR_fixed
+python test_taco_model_auto.py --checkpoint prev/blizzard_baseline_JE_EM_FFM_LCL.tacotron/taco_step350K_weights.pyt --model_name 4book_fixed

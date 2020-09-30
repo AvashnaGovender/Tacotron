cc341b59ca1f8f967a6c3178924e0deb3d91e5b0
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-09-30 13:57:39 +0200

    Update __init__.py

--- utils/text/__init__.py
+++ utils/text/__init__.py
@@ -33,6 +33,8 @@ def text_to_sequence(text, cleaner_names):
   for c in myseq:
       if c in symbols:
             sequence.append(symbols.index(c))
+
+  print(sequence)
   return sequence
 
   # Check for curly braces and treat their contents as ARPAbet:

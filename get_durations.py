3fc69d905f0023292d668ec96207cabe529cb748
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-10-05 14:44:01 +0200

    Update get_durations.py

--- get_durations.py
+++ get_durations.py
@@ -203,7 +203,10 @@ def main_work():
        mel_file = labfile.stem
        mel_features = np.load(f'{hp.data_path}/mel_dctts/{mel_file}.npy')
        audio_msec_length = mel_features.shape[1] * 50
-       audio_msec_length_12 = mel_features.shape[1] * 50
+
+       mel_features_12 = np.load(f'{hp.data_path}/mel/{mel_file}.npy')
+
+       audio_msec_length_12 = mel_features_12.shape[1] * 12.5
 
        resampled_lengths = resample_timings(lengths, from_rate=5.0, to_rate=50.0, total_duration=audio_msec_length)
        print(resampled_lengths)

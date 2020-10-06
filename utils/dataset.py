0f15733919ba82e7972e9c539a11013fbff4a71d
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-10-06 14:52:18 +0200

    Update dataset.py

--- utils/dataset.py
+++ utils/dataset.py
@@ -40,7 +40,7 @@ def get_vocoder_datasets(path: Path, batch_size, train_gta):
     dataset_ids = [x[0] for x in dataset]
 
     random.seed(1234)
-    random.shuffle(dataset_ids)
+    #random.shuffle(dataset_ids)
 
     test_ids = dataset_ids[-hp.voc_test_samples:]
     train_ids = dataset_ids[:-hp.voc_test_samples]

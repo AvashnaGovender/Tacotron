479ebfebbd99d84eda875caa46339de578650f4c
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-10-06 12:04:51 +0200

    Update gen_wavernn.py

--- gen_wavernn.py
+++ gen_wavernn.py
@@ -28,9 +28,9 @@ def gen_testset(model: WaveRNN, test_set, samples, batched, target, overlap, sav
             x = label_2_float(x, bits)
 
         save_wav(x, save_path/f'{k}k_steps_{i}_target.wav')
-
+        new_index = i+100
         batch_str = f'gen_batched_target{target}_overlap{overlap}' if batched else 'gen_NOT_BATCHED'
-        save_str = str(save_path/f'{i}_{k}k_steps_vocoded.wav')
+        save_str = str(save_path/f'{new_index}_{k}k_steps_vocoded.wav')
 
         _ = model.generate(m, save_str, batched, target, overlap, hp.mu_law)
 

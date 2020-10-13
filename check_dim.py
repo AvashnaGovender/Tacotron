f60e75b2723e68afcccbcc84efc909619759ed0a
Avashna Govender <38498073+AvashnaGovender@users.noreply.github.com>
2020-10-13 12:12:40 +0200

    Update check_dim.py

--- check_dim.py
+++ check_dim.py
@@ -18,14 +18,18 @@ text_inputs = f'{hp.data_path}/text_dict.pkl'
 mel_inputs = f'{hp.data_path}/dataset.pkl'
 
 with open(text_inputs, 'rb') as f:
-  content = pickle.load(f)
+  i_content = pickle.load(f)
 
-print("Text input dim:", len(content))
+print("Text input dim:", len(i_content))
 
 with open(mel_inputs, 'rb') as f:
-  content = pickle.load(f)
+  m_content = pickle.load(f)
 
-print("Mel input dim:", len(content))
 
-print(text_inputs[0])
-print(mel_inputs[0])
+print("Mel input dim:", len(m_content))
+
+dataset_ids_1 = [x for x in i_content]
+dataset_ids_2 = [x for x in m_content]
+
+print(dataset_ids_1[:3])
+print(dataset_ids_2[:3])
